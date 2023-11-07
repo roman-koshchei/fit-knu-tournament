a6ed672b24c7f5896ab9e27578c2599633e1d2db
Roman Koshchei <romankoshchei@gmail.com>
2023-11-07 17:05:57 +0200

    prepare project

---
+++ Data/Db.cs
@@ -0,0 +1,29 @@
+﻿using Data.Tables;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class Db : IdentityDbContext<User>
+{
+    public Db(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+    /// <summary>Save changes in database. But doesn't throw.</summary>
+    /// <returns>True if successful, false if not.</returns>
+    public async Task<bool> Save(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await SaveChangesAsync(cancellationToken);
+            return true;
+        }
+        catch { return false; }
+    }
+}
---
+++ Data/Tables/User.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Data.Tables;
+
+public class User : IdentityUser
+{
+}

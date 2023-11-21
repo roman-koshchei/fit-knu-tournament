c2f06e9d248acd5444dc5621e738a07f44259a4e
Bohdan <nicklchannel8@gmail.com>
2023-11-21 14:44:32 +0200

    User model information

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -2,6 +2,23 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+
+/* 
+User model has such fields:
+    Id: The unique identifier for the user.
+    UserName: The user's username.
+    NormalizedUserName: A normalized version of the username, used for case-insensitive comparisons and lookups.
+    Email: The user's email address.
+    NormalizedEmail: A normalized version of the email address, used for case-insensitive comparisons and lookups.
+    EmailConfirmed: A flag indicating whether the email address has been confirmed by the user.
+    PasswordHash: The hashed and salted password.
+    SecurityStamp: A random value that should be regenerated when a user changes their password, email, or other critical information.
+    ConcurrencyStamp: A value used to detect concurrent updates, which is typically updated whenever a user is modified.
+    TwoFactorEnabled: A flag indicating whether two-factor authentication is enabled for the user.
+    LockoutEnd: The date and time when the user's lockout period will end (if they are currently locked out).
+    LockoutEnabled: A flag indicating whether lockout is enabled for the user.
+    AccessFailedCount: The number of failed access attempts.
+ */
 namespace Api.Controllers
 {
     [Route("api/[controller]")]

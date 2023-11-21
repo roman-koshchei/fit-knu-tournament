986e87bce8e77cdc5772afb6c5705ac7f30e3dfe
Roman Koshchei <romankoshchei@gmail.com>
2023-11-21 14:12:42 +0200

    jwt

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,36 @@
+using Data;
+using Data.Tables;
+using Lib;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//JwtSecrets jwtSecrets = new(Secrets.JWT_ISSUER, Secrets.JWT_AUDIENCE, Secrets.JWT_SECRET);
+//builder.Services.AddScoped<Jwt>(_ => new(jwtSecrets));
+
+builder.Services
+        .AddIdentity<User, IdentityRole>()
+        .AddEntityFrameworkStores<Db>();
+//.AddDefaultTokenProviders()
+//.AddTokenProvider<DataProtectorTokenProvider<User>>("email");
+
+//builder.Services
+//    .AddAuthentication(options =>
+//    {
+//        options.DefaultAuthenticateScheme = RefreshOnly.Scheme;
+//        options.DefaultForbidScheme = RefreshOnly.Scheme;
+//        options.DefaultChallengeScheme = RefreshOnly.Scheme;
+//        options.DefaultScheme = RefreshOnly.Scheme;
+//    })
+//    .AddScheme<AuthenticationSchemeOptions, RefreshOnlyHandler>(RefreshOnly.Scheme, options => { });
 
 var app = builder.Build();
 
@@ -16,8 +41,18 @@ app.UseSwagger();
 app.UseSwaggerUI();
 //}
 
+app.UseCors(options =>
+{
+    options
+        .SetIsOriginAllowed(_ => true) // UseDomainCors handle it.
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+});
+
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Registration: POST email + pasword + confirm password -> return token
 
 Login: POST email + password -> return token
 
+Check: POST token -> ok if token valid
+
 Change password: POST token + old password + new password + change user version +1
 
-Change email: POST token + old password + new password + change user version + 1
+Change email: POST token + email + change user version + 1
+
+Delete user: DELET token + password
+
+Google: ?
+
+Apple: ?

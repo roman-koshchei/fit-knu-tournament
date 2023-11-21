35d0ae872e07b6a035cfad7a7ee96d5e7e4691dd
Bohdan <nicklchannel8@gmail.com>
2023-11-21 14:33:38 +0200

    Main controller for authentication

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET api/login ROSTIK
+        // - Logging in
+        [HttpGet("login")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/register BOHDAN
+        // - Registration
+        [HttpPost]
+        public void Register([FromBody] string value)
+        {
+        }
+
+        // PUT api/emailChange ROMA (Team lead big boss)
+        // - Updating email
+        [HttpPatch("{id}")]
+        public void UpdateEmail(int id, [FromBody] string value)
+        {
+        }
+
+        // PUT api/passwordChange
+        // - Updating password
+        [HttpPatch("{id}")]
+        public void UpdatePassword(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/deleteUser ROMA KORINEVSKII
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

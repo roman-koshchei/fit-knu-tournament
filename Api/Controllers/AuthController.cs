1b40a476eac2ca1c9b49ddaac54e920c55cbfa2e
Roman Koshchei <romankoshchei@gmail.com>
2023-11-21 17:09:53 +0200

    disable allow anonimus

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -7,9 +7,8 @@ using System.Security.Claims;
 
 namespace Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/auth")]
 [ApiController]
-[Authorize] // Add the Authorize attribute at the controller level to restrict access to authenticated users
 public class AuthController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
@@ -24,7 +23,6 @@ public class AuthController : ControllerBase
     // GET api/login ROSTIK
     // - Logging in
     [HttpPost("login")]
-    [AllowAnonymous] // Allow anonymous access for login
     public async Task<IActionResult> Login([FromBody] LoginModel model)
     {
         var user = await _userManager.FindByNameAsync(model.UserName);
@@ -42,7 +40,6 @@ public class AuthController : ControllerBase
     // POST api/register BOHDAN
     // - Registration
     [HttpPost("register")]
-    [AllowAnonymous] // Allow anonymous access for registration
     public async Task<IActionResult> Register([FromBody] RegisterModel model)
     {
         var existingUser = await _userManager.FindByNameAsync(model.UserName);
@@ -64,40 +61,9 @@ public class AuthController : ControllerBase
         return BadRequest(result.Errors);
     }
 
-    // PUT api/emailChange ROMA (Team lead big boss)
-    // - Updating email
-    [HttpPatch("updateEmail")]
-    public async Task<IActionResult> UpdateEmail([FromBody] UpdateEmailModel model)
-    {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // Get the user id from the authenticated user's claims
-
-        var user = await _userManager.FindByIdAsync(userId);
-
-        if (user == null)
-        {
-            return NotFound("User not found");
-        }
-
-        // Check if the authenticated user is the owner of the account
-        if (user.Id != userId)
-        {
-            return Forbid(); // Return 403 Forbidden if the user is not the owner
-        }
-
-        user.Email = model.NewEmail;
-        user.NormalizedEmail = model.NewEmail.ToUpper();
-        var result = await _userManager.UpdateAsync(user);
-
-        if (result.Succeeded)
-        {
-            return Ok("Email updated successfully");
-        }
-
-        return BadRequest(result.Errors);
-    }
-
     // DELETE api/deleteUser ROMA KORINEVSKII
     [HttpDelete("deleteUser")]
+    [Authorize]
     public async Task<IActionResult> Delete()
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -137,9 +103,4 @@ public class RegisterModel
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-}
-
-public class UpdateEmailModel
-{
-    public string NewEmail { get; set; }
 }

0c06bc90e3e87e8ddb4927dd969a486eeac516be
Roman Koshchei <romankoshchei@gmail.com>
2023-11-22 10:51:01 +0200

    move

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -10,13 +10,11 @@ namespace Api.Controllers;
 [ApiController]
 public class AuthController : ControllerBase
 {
-    private readonly SignInManager<User> _signInManager;
     private readonly UserManager<User> _userManager;
     private readonly Jwt _jwt;
 
-    public AuthController(SignInManager<User> signInManager, UserManager<User> userManager, Jwt jwt)
+    public AuthController(UserManager<User> userManager, Jwt jwt)
     {
-        _signInManager = signInManager;
         _userManager = userManager;
         _jwt = jwt;
     }
@@ -53,66 +51,6 @@ public class AuthController : ControllerBase
         var token = _jwt.Token(newUser.Id, newUser.Version);
 
         return Ok(new { Token = token, UserName = newUser.UserName, Email = newUser.Email });
-    }
-
-    [HttpPost("google-login")]
-    public IActionResult ExternalLogin()
-    {
-        var properties = _signInManager.ConfigureExternalAuthenticationProperties("google", Url.Action("GoogleLoginCallback"));
-        return Challenge(properties, "Google");
-    }
-
-    [HttpGet("google-login-callback")]
-    public async Task<IActionResult> GoogleLoginCallback(string? remoteError = null)
-    {
-        if (remoteError != null)
-        {
-            return BadRequest($"Error from external provider: {remoteError}");
-        }
-
-        var info = await _signInManager.GetExternalLoginInfoAsync();
-        if (info == null)
-        {
-            return BadRequest("Error loading external login information.");
-        }
-
-        // Check if a user with the Google-registered email exists
-        var user = await _userManager.FindByEmailAsync(info.Principal.FindFirstValue(ClaimTypes.Email));
-
-        if (user != null)
-        {
-            // User exists, sign them in
-            await _signInManager.SignInAsync(user, isPersistent: false);
-            var token = _jwt.Token(user.Id, user.Version);
-
-            // Return the response as JSON
-            return Ok(new { Token = token, UserName = user.UserName, Email = user.Email });
-        }
-        else
-        {
-            // User does not exist, create a new user
-            var newUser = new User(email: info.Principal.FindFirstValue(ClaimTypes.Email));
-
-            var result = await _userManager.CreateAsync(newUser);
-            if (!result.Succeeded)
-            {
-                return BadRequest("Error creating a new user.");
-            }
-
-            // Add the external login for the user
-            result = await _userManager.AddLoginAsync(newUser, info);
-            if (!result.Succeeded)
-            {
-                return BadRequest("Error adding external login to the new user.");
-            }
-
-            // Sign in the newly created user
-            await _signInManager.SignInAsync(newUser, isPersistent: false);
-            var token = _jwt.Token(newUser.Id, newUser.Version);
-
-            // Return the response as JSON
-            return Ok(new { Token = token, UserName = newUser.UserName, Email = newUser.Email });
-        }
     }
 
     // Models for input data

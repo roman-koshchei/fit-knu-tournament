04c8e72a082bc595128b9d10c6812fa391075784
Bohdan <nicklchannel8@gmail.com>
2023-11-22 23:33:33 +0200

    Incorrect version of RegisterController.cs

--- Web/Controllers/RegisterController.cs
+++ Web/Controllers/RegisterController.cs
@@ -1,35 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Tables;
+using Lib;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Web.Models;
 
-namespace Web.Controllers;
-
-public class RegisterController : Controller
+namespace Web.Controllers
 {
-    public IActionResult Index()
+    public class RegisterController : Controller
     {
-        return View();
-    }
+        private readonly UserManager<User> _userManager;
+        private readonly Jwt _jwt;
 
-    [HttpPost]
-    public IActionResult Post(RegisterViewModel model)
-    {
-        if (ModelState.IsValid)
+        public RegisterController(UserManager<User> userManager, Jwt jwtService)
         {
-            if (model.Password != model.ConfirmPassword)
-            {
-                ModelState.AddModelError("ConfirmPassword", "Passwords do not match");
-                return View(model);
-            }
-            // если пароли совпадают, сохранить пользователя, позже проделаем
-
-            return RedirectToAction("Index", "Account");
+            _userManager = userManager;
+            _jwt = jwtService;
         }
-        return View(model);
-    }
 
-    [HttpPost]
-    public async Task<IActionResult> LoginPost()
-    {
-        return Ok();
+        public void AddAuthCookie(string token)
+        {
+            Response.Cookies.Append("token", token, new()
+            {
+                HttpOnly = true,
+                Secure = true,
+                Expires = DateTimeOffset.Now.AddDays(30)
+            });
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Password != model.ConfirmPassword)
+                {
+                    ModelState.AddModelError("ConfirmPassword", "Passwords do not match");
+                    return BadRequest("Passwords do not match");
+                }
+
+                var existingUser = await _userManager.FindByEmailAsync(model.Email);
+                if (existingUser != null)
+                {
+                    return Conflict("Email is already taken");
+                }
+
+                var newUser = new User(model.Email);
+
+                var result = await _userManager.CreateAsync(newUser, model.Password);
+                if (!result.Succeeded)
+                {
+                    // Handle identity errors
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return StatusCode(500, ModelState);
+                }
+
+                // User created successfully, generate authentication token
+                var token = _jwt.Token(newUser.Id, newUser.Version);
+
+                AddAuthCookie(token);
+
+                return View("Success");
+            }
+
+            // If ModelState is not valid, return bad request with ModelState errors
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginPost()
+        {
+            // Implement your login logic here
+            // This method should handle user login
+            return Ok();
+        }
     }
 }

621359ec1ac20de029b5d04b50950b1fcab3a0dc
Roman Koshchei <romankoshchei@gmail.com>
2023-11-22 15:42:19 +0200

    ui

---
+++ Web/Controllers/Api/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Data.Tables;
+using Lib;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<User> userManager;
+    private readonly Jwt jwt;
+
+    public AuthController(UserManager<User> userManager, Jwt jwt)
+    {
+        this.userManager = userManager;
+        this.jwt = jwt;
+    }
+
+    [HttpGet("login")]
+    public async Task<IActionResult> Login()
+    {
+        return Ok();
+    }
+}

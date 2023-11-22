b1a713c550d926128b9ef4bf61666c435aa54ba5
Roman Koshchei <romankoshchei@gmail.com>
2023-11-22 14:54:01 +0200

    cleanup

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,63 +1,59 @@
 ﻿using Data.Tables;
 using Lib;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
+namespace Api.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    [Route("api/auth")]
-    [ApiController]
-    public class AuthController : ControllerBase
+    private readonly UserManager<User> userManager;
+    private readonly Jwt jwt;
+
+    public AuthController(UserManager<User> userManager, Jwt jwt)
     {
-        private readonly SignInManager<User> _signInManager;
-        private readonly UserManager<User> _userManager;
-        private readonly Jwt _jwt;
-
-        public AuthController(SignInManager<User> signInManager, UserManager<User> userManager, Jwt jwt)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _jwt = jwt;
-        }
-
-        [HttpGet("login")]
-        public async Task<IActionResult> Login([FromBody] LoginModel model)
-        {
-            var user = await _userManager.FindByNameAsync(model.UserName);
-
-            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
-            {
-                return Unauthorized("Invalid credentials");
-            }
-
-            var token = _jwt.Token(user.Id, user.Version);
-
-            return Ok(new { Token = token, UserName = user.UserName, Email = user.Email });
-        }
-
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterModel model)
-        {
-            var existingUser = await _userManager.FindByNameAsync(model.UserName);
-            if (existingUser != null)
-            {
-                return BadRequest("Username already exists");
-            }
-
-            var newUser = new User(model.Email);
-
-            var result = await _userManager.CreateAsync(newUser, model.Password);
-            if (!result.Succeeded) return BadRequest(result.Errors);
-
-            var token = _jwt.Token(newUser.Id, newUser.Version);
-
-            return Ok(new { Token = token, UserName = newUser.UserName, Email = newUser.Email });
-        }
-
-        // Models for input data
-        public record LoginModel(string UserName, string Password);
-
-        public record RegisterModel(string UserName, string Email, string Password);
+        this.userManager = userManager;
+        this.jwt = jwt;
     }
+
+    [HttpGet("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var user = await userManager.FindByNameAsync(model.UserName);
+
+        if (user == null || !await userManager.CheckPasswordAsync(user, model.Password))
+        {
+            return Unauthorized("Invalid credentials");
+        }
+
+        var token = jwt.Token(user.Id, user.Version);
+
+        return Ok(new { Token = token, UserName = user.UserName, Email = user.Email });
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+        var existingUser = await userManager.FindByNameAsync(model.UserName);
+        if (existingUser != null)
+        {
+            return BadRequest("Username already exists");
+        }
+
+        var newUser = new User(model.Email);
+
+        var result = await userManager.CreateAsync(newUser, model.Password);
+        if (!result.Succeeded) return BadRequest(result.Errors);
+
+        var token = jwt.Token(newUser.Id, newUser.Version);
+
+        return Ok(new { Token = token, UserName = newUser.UserName, Email = newUser.Email });
+    }
+
+    // Models for input data
+    public record LoginModel(string UserName, string Password);
+
+    public record RegisterModel(string UserName, string Email, string Password);
 }

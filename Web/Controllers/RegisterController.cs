551d5d49f9451acf6116a666c582e467cf11bfdc
Roman Koshchei <romankoshchei@gmail.com>
2023-11-23 11:51:38 +0200

    ix

--- Web/Controllers/RegisterController.cs
+++ Web/Controllers/RegisterController.cs
@@ -9,13 +9,13 @@ namespace Web.Controllers;
 
 public class RegisterController : Controller
 {
-    private readonly UserManager<User> _userManager;
-    private readonly Jwt _jwt;
+    private readonly UserManager<User> userManager;
+    private readonly Jwt jwt;
 
-    public RegisterController(UserManager<User> userManager, Jwt jwtService)
+    public RegisterController(UserManager<User> userManager, Jwt jwt)
     {
-        _userManager = userManager;
-        _jwt = jwtService;
+        this.userManager = userManager;
+        this.jwt = jwt;
     }
 
     public IActionResult Index()
@@ -34,7 +34,7 @@ public class RegisterController : Controller
                 return BadRequest("Passwords do not match");
             }
 
-            var existingUser = await _userManager.FindByEmailAsync(model.Email);
+            var existingUser = await userManager.FindByEmailAsync(model.Email);
             if (existingUser != null)
             {
                 ModelState.AddModelError("Email", "Email already registered");
@@ -43,7 +43,7 @@ public class RegisterController : Controller
 
             var newUser = new User(model.Email);
 
-            var result = await _userManager.CreateAsync(newUser, model.Password);
+            var result = await userManager.CreateAsync(newUser, model.Password);
             if (!result.Succeeded)
             {
                 // Handle identity errors
@@ -55,7 +55,7 @@ public class RegisterController : Controller
             }
 
             // User created successfully, generate authentication token
-            var token = _jwt.Token(newUser.Id, newUser.Version);
+            var token = jwt.Token(newUser.Id, newUser.Version);
 
             Response.AddAuthCookie(token);
 
